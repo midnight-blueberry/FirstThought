339a0dbff7e4d7ffbe68828ee6b6706f365db6a9
AutoThinker <olga.climova.1995@gmail.com>
2025-08-12 02:37:32 +0300

    feat: dark theme styling for diary list

--- components/ui/organisms/diary-list.tsx
+++ components/ui/organisms/diary-list.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {
   FlatList,
   StyleSheet,
-  Text,
   View
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { useTheme } from 'styled-components/native';
+import AppText from '../atoms/app-text';
 
 interface Diary {
   id: string;
@@ -18,39 +19,52 @@ type DiaryListProps = {
 };
 
 const DiaryList: React.FC<DiaryListProps> = ({ data }: DiaryListProps) => {
+  const theme = useTheme();
 
   const renderItem = ({ item }: { item: Diary }) => (
-    <View style={styles.itemContainer}>
-      <Ionicons name={item.icon} size={24} />
-      <Text style={styles.itemText}>{item.title}</Text>
+    <View
+      style={[
+        styles.itemContainer,
+        {
+          padding: theme.spacing.large,
+          backgroundColor: theme.colors.background,
+          borderRadius: theme.borderRadius,
+          borderColor: theme.colors.primary,
+          borderWidth: theme.borderWidth,
+          marginBottom: theme.spacing.medium,
+        },
+      ]}
+    >
+      <Ionicons
+        name={item.icon}
+        size={theme.iconSize.small}
+        color={theme.colors.text}
+      />
+      <AppText style={{ marginLeft: theme.spacing.large }} color="text">
+        {item.title}
+      </AppText>
     </View>
   );
 
   return (
-      <FlatList
-        data={data}
-        keyExtractor={item => item.id}
-        renderItem={renderItem}
-        contentContainerStyle={styles.listContent}
-      />
-  )
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={renderItem}
+      contentContainerStyle={{
+        paddingLeft: theme.spacing.medium,
+        paddingRight: theme.padding.small,
+      }}
+      style={{ backgroundColor: theme.colors.background }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-  listContent: {
-  },
   itemContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 12,
-    backgroundColor: '#fff',
-    borderRadius: 8,
-    marginBottom: 8
   },
-  itemText: {
-    marginLeft: 12,
-    fontSize: 16
-  }
-}); 
+});
 
 export default DiaryList;
