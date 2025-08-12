d6c19de4154640d8773653a6b4c0c6766f9e2a76
AutoThinker <olga.climova.1995@gmail.com>
2025-08-12 20:14:50 +0300

    refactor: rename yellow base color

--- theme/colors.ts
+++ theme/colors.ts
@@ -1,7 +1,7 @@
 const baseColors = {
   white: '#FFFFFF',
   black: '#000000',
-  yellow: '#FFCD00',
+  accentColor: '#FFCD00',
   gray: '#999999',
 } as const;
 
@@ -17,7 +17,7 @@ export interface ColorTokens {
 }
 
 export const lightColors: ColorTokens = {
-  primary: baseColors.yellow,
+  primary: baseColors.accentColor,
   primaryText: baseColors.black,
   secondary: baseColors.black,
   secondaryText: baseColors.white,
@@ -28,12 +28,12 @@ export const lightColors: ColorTokens = {
 };
 
 export const darkColors: ColorTokens = {
-  primary: baseColors.yellow,
+  primary: baseColors.accentColor,
   primaryText: baseColors.black,
   secondary: baseColors.black,
-  secondaryText: baseColors.yellow,
+  secondaryText: baseColors.accentColor,
   background: baseColors.black,
-  text: baseColors.yellow,
+  text: baseColors.accentColor,
   disabled: baseColors.gray,
   disabledText: baseColors.gray,
 };
