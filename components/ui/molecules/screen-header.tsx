805e579277c15732a1ba25b169ffab5786ea11f6
AutoThinker <olga.climova.1995@gmail.com>
2025-08-13 20:17:13 +0300

    feat(settings): add header shadow (#36)
    * feat(settings): add header shadow
    
    * style(settings): darken header shadow
    
    * feat(settings): theme-aware header shadow
    
    * fix(settings): set constant header shadow opacity

--- components/ui/molecules/screen-header.tsx
+++ components/ui/molecules/screen-header.tsx
@@ -22,8 +22,12 @@ const ScreenHeader: React.FC<ScreenHeaderProps> = ({ title, onBack, saveOpacity 
 
   const containerStyle = {
     paddingVertical: theme.padding.small,
-    borderBottomWidth: theme.borderWidth.small,
-    borderBottomColor: theme.colors.basic,
+    backgroundColor: theme.colors.background,
+    shadowColor: theme.colors.basic,
+    shadowOffset: { width: 0, height: theme.borderWidth.small },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 4,
   } as const;
 
   const leftStyle = {
