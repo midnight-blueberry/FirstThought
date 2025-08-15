e5bdd0e878d7735bf74921ac77c0515e49abd22d
AutoThinker <olga.climova.1995@gmail.com>
2025-08-15 11:43:51 +0300

    chore: inline menu icon

--- components/ui/organisms/header.tsx
+++ components/ui/organisms/header.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { DrawerActions } from '@react-navigation/native';
 import { useNavigation } from 'expo-router';
 import { useTheme } from 'styled-components/native';
-import MenuIcon from '../atoms/menu-icon';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import SearchField from '../molecules/input-field';
 
 const Header: React.FC = () => {
@@ -15,7 +15,7 @@ const Header: React.FC = () => {
   return (
     <View style={[styles.header, { backgroundColor: theme.colors.background }]}>
       <TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.openDrawer())} hitSlop={8}>
-        <MenuIcon />
+        <Ionicons name="menu" size={theme.iconSize.large} color={theme.colors.basic} />
       </TouchableOpacity>
       <SearchField placeholder="Поиск по всем дневникам..." />
     </View>
