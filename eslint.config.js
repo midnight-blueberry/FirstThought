43316b0c9774d33e10d3a7088e494efc2f10539c
CozyCode95 <olga.climova.1995@gmail.com>
2025-08-15 10:43:13 +0300

    Add eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import tseslint from 'typescript-eslint';
 
-module.exports = defineConfig([
-  expoConfig,
+export default tseslint.config(
+  // Другие твои конфиги, если есть
   {
-    ignores: ['dist/*'],
-  },
-]);
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json', // важно для проверки типов
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    rules: {
+      // Рекомендованные правила с проверкой типов
+      ...tseslint.configs.recommendedTypeChecked.rules,
+      // (опционально) без строгих проверок стиля
+    },
+  }
+);
