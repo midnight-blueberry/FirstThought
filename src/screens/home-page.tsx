d290e8511e1a2399bf8e04d199d7f1ccb98361f0
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-08-23 20:36:29 +0300

    refactor: move screens to src

---
+++ src/screens/home-page.tsx
@@ -0,0 +1,11 @@
+import HomePage from '@components/pages/HomePage';
+import PageContainer from '@components/common/PageContainer';
+
+export default function HomePageScreen() {
+  return (
+    <PageContainer>
+      <HomePage />
+    </PageContainer>
+  );
+}
+
