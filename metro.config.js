3e4f38b3097d724e5f04eccd7fbc0b8cf0ea2e90
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-08-23 15:42:37 +0300

    chore: alias edge-to-edge module

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,12 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  'react-native-is-edge-to-edge': path.resolve(__dirname, 'src/polyfills/react-native-is-edge-to-edge.ts'),
+config.resolver.alias = {
+  ...(config.resolver.alias || {}),
+  'react-native-is-edge-to-edge': path.resolve(
+    __dirname,
+    'src/polyfills/react-native-is-edge-to-edge.ts',
+  ),
 };
 
 module.exports = config;
