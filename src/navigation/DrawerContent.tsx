8b7320c436962467f6c2853c5dfd36c56fd379b9
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-08-25 13:47:57 +0300

    refactor: update DrawerContent typing

--- src/navigation/DrawerContent.tsx
+++ src/navigation/DrawerContent.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-import type { DrawerContentComponentProps } from '@react-navigation/drawer';
+import {
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
 import useTheme from '@hooks/useTheme';
-import type { DrawerParamList } from './types';
 
 export default function DrawerContent(
-  props: DrawerContentComponentProps<DrawerParamList>,
+  props: DrawerContentComponentProps,
 ) {
   const theme = useTheme();
   return (
