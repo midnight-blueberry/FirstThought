d623e20cf44cfc76b8e1b9283811a796e715acb1
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-08-26 19:01:09 +0300

    refactor: replace font family keys with literals (#216)

--- src/constants/fonts/weights.ts
+++ src/constants/fonts/weights.ts
@@ -1,14 +1,13 @@
 import type { FontFamily, FontWeight } from './types';
-import { FAMILIES } from './families';
 
 export const WEIGHTS = {
-  [FAMILIES.Bad_Script]: ['400'],
-  [FAMILIES.Comfortaa]: ['300', '400', '500', '600', '700'],
-  [FAMILIES.Lora]: ['400', '500', '600', '700'],
-  [FAMILIES.Montserrat]: ['300', '400', '500', '600', '700'],
-  [FAMILIES.Nata_Sans]: ['300', '400', '500', '600', '700'],
-  [FAMILIES.PT_Sans]: ['400', '700'],
-  [FAMILIES.Raleway]: ['300', '400', '500', '600', '700'],
-  [FAMILIES.Roboto_Condensed]: ['300', '400', '500', '600', '700'],
-  [FAMILIES.Roboto_Slab]: ['300', '400', '500', '600', '700'],
+  Bad_Script: ['400'],
+  Comfortaa: ['300', '400', '500', '600', '700'],
+  Lora: ['400', '500', '600', '700'],
+  Montserrat: ['300', '400', '500', '600', '700'],
+  Nata_Sans: ['300', '400', '500', '600', '700'],
+  PT_Sans: ['400', '700'],
+  Raleway: ['300', '400', '500', '600', '700'],
+  Roboto_Condensed: ['300', '400', '500', '600', '700'],
+  Roboto_Slab: ['300', '400', '500', '600', '700'],
 } as const satisfies Record<FontFamily, ReadonlyArray<FontWeight>>;
