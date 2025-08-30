e414a37f86cc4ca5c369e761ee005b1ceae80224
CozyCode95 <olga.climova.1995@gmail.com>
2025-08-30 23:45:19 +0300

    Fix font weights array

--- src/constants/fonts/weights.ts
+++ src/constants/fonts/weights.ts
@@ -2,12 +2,12 @@ import type { FontFamily, FontWeight } from './types';
 
 export const FONT_WEIGHTS_BY_FAMILY: Record<FontFamily, readonly FontWeight[]> = {
   Bad_Script: [400],
-  Comfortaa: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  Lora: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  Montserrat: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  Nata_Sans: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  Comfortaa: [300, 400, 500, 600, 700],
+  Lora: [400, 500, 600, 700],
+  Montserrat: [300, 400, 500, 600, 700],
+  Nata_Sans: [300, 400, 500, 600, 700],
   PT_Sans: [400, 700],
-  Raleway: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  Roboto_Condensed: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  Roboto_Slab: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  Raleway: [300, 400, 500, 600, 700],
+  Roboto_Condensed: [300, 400, 500, 600, 700],
+  Roboto_Slab: [300, 400, 500, 600, 700],
 } as const;
