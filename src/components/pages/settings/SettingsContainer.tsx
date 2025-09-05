06785d4690dd03dcc99b7d79eb5aefb72dd501bf
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-09-05 13:31:11 +0300

    Scope save indicator to settings header and cleanup on unmount

--- src/components/pages/settings/SettingsContainer.tsx
+++ src/components/pages/settings/SettingsContainer.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SettingsContent from './SettingsContent';
 import useSettingsVm from './useSettingsVm';
+import { useSaveIndicator } from '@components/header/SaveIndicator';
 
 export default function SettingsContainer() {
   const vm = useSettingsVm();
+  const { hide } = useSaveIndicator();
+  useEffect(() => {
+    return () => {
+      hide();
+    };
+  }, [hide]);
   return <SettingsContent {...vm} />;
 }
