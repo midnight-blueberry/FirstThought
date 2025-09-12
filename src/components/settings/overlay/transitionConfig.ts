9b7176271c97abecb4a7366fd85b7a914e1590de
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-09-12 13:05:03 +0300

    chore: add overlay transition constants

---
+++ src/components/settings/overlay/transitionConfig.ts
@@ -0,0 +1,10 @@
+import { Easing } from 'react-native';
+
+export const OVERLAY_SHOW_DURATION_MS = 500;
+export const OVERLAY_HIDE_DURATION_MS = 500;
+export const OVERLAY_MAX_OPACITY = 1;
+export const OVERLAY_MIN_OPACITY = 0;
+export const OVERLAY_EASING_IN = Easing.inOut(Easing.cubic);
+export const OVERLAY_EASING_OUT = Easing.inOut(Easing.cubic);
+export const OVERLAY_OPAQUE_TIMEOUT_MS = 300;
+export const OVERLAY_POINTER_EVENTS_THRESHOLD = 0.01;
