f0cb88b48499776b4688b1db2d79392105f1022f
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-09-12 15:13:18 +0300

    feat: expose overlay barrel (#347)

--- src/components/settings/overlay/OverlayTransition.tsx
+++ src/components/settings/overlay/OverlayTransition.tsx
@@ -167,3 +167,5 @@ export function useOverlayTransition() {
   return ctx;
 }
 
+export default OverlayTransitionProvider;
+
---
+++ src/components/settings/overlay/index.ts
@@ -0,0 +1,5 @@
+export { default as OverlayTransition } from './OverlayTransition';
+export * from './OverlayTransition';
+export { useOverlayAnimation } from './useOverlayAnimation';
+export * from './transitionConfig';
+
