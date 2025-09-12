d8d2c1a8756ffe43d069526ebeeb136673d6f6c5
CozyCode95 <olga.climova.1995@gmail.com>
2025-09-12 17:32:54 +0300

    fix: Jest config file

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+// jest.config.ts
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+};
+export default config;
