b31ab79a15803afd8c489ae77d5a97e4b9472f0a
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-09-14 17:51:03 +0300

    test: wrap sticky scroll render in act

--- tests/mocks/react-test-renderer.ts
+++ tests/mocks/react-test-renderer.ts
@@ -1,9 +1,17 @@
+import React from 'react';
+// @ts-ignore
+import ReactDOMServer from 'react-dom/server';
+
 export const act = async (cb: () => Promise<any> | void) => {
   await cb();
 };
 
-export const create = (..._args: any[]) => ({
-  toJSON: () => null,
-});
+export const create = (element: React.ReactElement) => {
+  ReactDOMServer.renderToString(element);
+  return {
+    toJSON: () => null,
+    unmount: () => {},
+  };
+};
 
 export default { create, act };
