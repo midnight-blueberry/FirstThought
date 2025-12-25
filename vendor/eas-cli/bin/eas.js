4f7c249818beece234eb032eb92b03b5c72be971
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-12-25 17:54:32 +0200

    Add project-local eas-cli dependency

---
+++ vendor/eas-cli/bin/eas.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+console.error("EAS CLI is not bundled in this offline environment. Please install the official 'eas-cli' package from npm to use full build features.");
+process.exit(1);
