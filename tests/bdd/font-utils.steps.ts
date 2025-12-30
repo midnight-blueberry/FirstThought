d8465273f1d1b1de9e9270807b8bb5103a8d65ee
Midnight Blueberry <olga.climova.1995@gmail.com>
2025-12-30 15:45:54 +0200

    Add base font name BDD scenario (#484)
    * Add base font name BDD scenario
    
    * Add unit-tests to basic-checks to run everything in 1 step

--- tests/bdd/font-utils.steps.ts
+++ tests/bdd/font-utils.steps.ts
@@ -1,4 +1,4 @@
-import { toFamilyKey } from '@/utils/font';
+import { getBaseFontName, toFamilyKey } from '@/utils/font';
 import type { JestCucumberTestFn, StepDefinitions } from '@tests/bdd/bddTypes';
 
 export default (test: JestCucumberTestFn) => {
@@ -16,6 +16,23 @@ export default (test: JestCucumberTestFn) => {
 
     then('it equals "Bad_Script"', () => {
       expect(result).toBe('Bad_Script');
+    });
+  });
+
+  test('getBaseFontName strips weight suffix and converts underscores to spaces', ({ given, when, then }: StepDefinitions) => {
+    let fontName = '';
+    let result = '';
+
+    given('a theme font name "Bad_Script-400-italic"', () => {
+      fontName = 'Bad_Script-400-italic';
+    });
+
+    when('I get the base font name', () => {
+      result = getBaseFontName(fontName);
+    });
+
+    then('it equals "Bad Script"', () => {
+      expect(result).toBe('Bad Script');
     });
   });
 };
