c26c261a4f2b306e0b178f12c93e789e5cbe0360
Midnight Blueberry <olga.climova.1995@gmail.com>
2026-02-09 11:05:30 +0200

    Fix Jest @settings alias

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@screens/(.*)$': '<rootDir>/src/screens/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@settings/(.*)$': '<rootDir>/src/settings/$1',
     '^@constants/(.*)$': '<rootDir>/src/constants/$1',
     '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^@theme/(.*)$': '<rootDir>/src/theme/$1',
